7eb75b00b1bbed225538e7759e458d2ba097c659
toznl <dontsay_no@nate.com>
2022-04-01 09:52:27 +0900

    init

--- ScanProgram/obj/Debug/netcoreapp3.0/MainWindow.g.i.cs
+++ ScanProgram/obj/Debug/netcoreapp3.0/MainWindow.g.i.cs
@@ -302,7 +302,7 @@ namespace ScanProgram {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/ScanProgram;component/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/ScanProgram;V1.0.0.0;component/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
